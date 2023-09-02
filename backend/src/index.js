fb09f65b7dda0ec1c272526267246a5337ce7e2b
Francis <francisseneve@gmail.com>
2023-09-02 09:24:34 +0100

    Initial project

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, ()=>console.log("Server is up and running..."));
+
+
