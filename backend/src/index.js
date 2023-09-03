0552673658ef0e0689677aa5c997ba0d36bdcf80
Francis <francisseneve@gmail.com>
2023-09-03 11:44:46 +0100

    Custom logger

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 import app from './app.js';
+import logger from './config/logger.config.js';
 
 
 const PORT = process.env.PORT || 5000
 
 
-app.listen(PORT, ()=>console.log("Server is up and running..."));
+app.listen(PORT, ()=>logger.info("Server is up and running..."));
 
 
